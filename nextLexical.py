7927720e2db3fe20948c3a2b06310d05944814d2
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-07-19 22:48:53 +0530

    Create nextLexical.py

---
+++ nextLexical.py
@@ -0,0 +1,28 @@
+"""
+nextLexical
+"""
+
+def nextLexical(arr):
+	if arr is None:
+		return
+	n=len(arr)
+	l_arr=list(arr)
+	pivot=-1
+	for i in range(n-2,-1,-1):
+		if l_arr[i]<l_arr[i+1]:
+			pivot=i
+			break
+	if pivot==-1:
+		return arr
+	result=l_arr[:pivot]
+	l_arr[i],l_arr[i+1]=l_arr[i+1],l_arr[i]
+	result.append(l_arr[pivot])
+	result.extend(sorted(l_arr[pivot+1:]))
+	return "".join(result)
+
+arr="ATLAS"
+print(nextLexical(arr))
+arr="COCHIN"
+print(nextLexical(arr))
+arr="ALPHA"
+print(nextLexical(arr))
