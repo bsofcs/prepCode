5e6d1b77e777ec7429b410ab70c095502949ad9b
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-07-22 02:04:01 +0530

    Update longestSubsequenceInTwoString.py

--- longestSubsequenceInTwoString.py
+++ longestSubsequenceInTwoString.py
@@ -4,7 +4,7 @@ longestSubsequenceInTwoString
 def longestSubsequenceInTwoString(arr1,arr2):
 	if arr1 is None or arr2 is None:
 		return None
-	Table=[[0 for _ in range(m+1)] for _ in range(n+1)]
+	Table=[[0 for _ in range(len(arr2)+1)] for _ in range(len(arr1)+1)]
 	for i,x in enumerate(arr1):
 		for j,y in enumerate(arr2):
 			if x==y:
@@ -15,13 +15,18 @@ def longestSubsequenceInTwoString(arr1,arr2):
 	x=len(arr1);y=len(arr2)
 	while x!=0 and y!=0:
 		if Table[x][y]==Table[x-1][y]:
+			print("from (",x,",",y,") to (",x-1,",",y,")")
 			x-=1
 		elif Table[x][y]==Table[x][y-1]:
+			print("from (",x,",",y,") to (",x,",",y-1,")")
 			y-=1
 		else:
-			assert X[x-1]==Y[y-1]
-			result=X[x-1]+result
+			print("from (",x,",",y,") to (",x-1,",",y-1,")")
+			assert arr1[x-1]==arr2[y-1]
+			result=arr1[x-1]+result
 			x-=1;y-=1
+	for i in range(len(Table)):
+		print(Table[i])
 	return result
 
 if __name__=='__main__':
