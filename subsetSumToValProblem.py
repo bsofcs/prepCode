1ed1a8f9b6dbde98627c889c5027e4e3fa1327be
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-09-09 02:32:58 +0530

    Update subsetSumToValProblem.py

--- subsetSumToValProblem.py
+++ subsetSumToValProblem.py
@@ -2,6 +2,7 @@
 subsetSumToValProblem
 Let us assume that "arr" is an array of interger and "T" is an natural number.
 Check if there is a subset of the array which sums up to the value T.
+Return Number of such subset if we have any.
 """
 
 def subsetUtil(arr,n,T):
@@ -48,9 +49,29 @@ def subsetSumToVal(arr,T):
 def countSubsetSumToVal(arr,T):
 	if arr is None or T is None:
 		return None
+	n=len(arr)
+	m=[[0 for i in range(T+1)] for j in range(n+1)]
+	for i in range(n+1):
+		m[i][0]=1
+	for i in range(1,n+1):
+		for j in range(1,T+1):
+			if arr[i-1]<=j:
+				m[i][j]=m[i-1][j]+m[i-1][j-arr[i-1]]
+			else:
+				m[i][j]=m[i-1][j]
+	for i in range(n+1):
+		print(m[i])
+	return m[n][T]
 
-
-arr=[1,6,1,3]
+arr=[1,1,3,2,6]
 T=5
 print(subsetSumToValBT(arr,T))
 print(subsetSumToVal(arr,T))
+print("Count:",countSubsetSumToVal(arr,T))
+
+
+arr=[3,3,3,3]
+T=6
+print(subsetSumToValBT(arr,T))
+print(subsetSumToVal(arr,T))
+print("Count:",countSubsetSumToVal(arr,T))
