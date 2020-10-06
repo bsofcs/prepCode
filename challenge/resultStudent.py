b10b655f995f40914f6db02b43bffb7b30af751a
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-10-06 20:32:37 +0530

    Create resultStudent.py

---
+++ challenge/resultStudent.py
@@ -0,0 +1,5 @@
+"""
+resultStudent
+Given a result of a competition among all the students of a class, write a program to make students stand in a order such that every student must have lost to the student in his/her immediate left and won against the student to his/her immediate right
+"""
+
