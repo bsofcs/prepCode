78f57769e189c829d2bc98c9584b34b109c95035
bsofcs <42695433+bsofcs@users.noreply.github.com>
2022-05-23 09:52:44 +0530

    Update allPossibleSubset.py

--- DP/allPossibleSubset.py
+++ DP/allPossibleSubset.py
@@ -1,17 +1,41 @@
 """
 allPossibleSubset
 """
-a=[1,4,2,5]
-n=len(a)
-allPoss=[[]]
-for i in range(2**n):
-	tmp=[]
-	a_i=(list(bin(i))[2:])[::-1]
-	for j in range(len(a_i)):
-		if a_i[j]=="1":
-			tmp.append(a[n-1-j])
-	if len(tmp):
-		allPoss.append(tmp[::-1])
-print(sorted(allPoss))
-print(len(allPoss))
-	
+class Solution:
+    def subsets(self, nums):
+        if nums is None:
+            return None
+        n=len(nums)
+        ni=2**n
+        result=[]
+        for i in range(ni):
+            #con=list(bin(i))[2:]
+            #while(len(con)<n):
+            #    con.insert(0,'0')
+            con=list(bin(i)[2:].zfill(n))       #very Important zfill which does left padding
+            tmp=[]
+            print(con)
+            jn=len(con)
+            for j in range(jn):
+                print(j,con[j],nums[j])
+                if con[j]=='1':
+                    tmp.append(nums[j])
+            print("TMP:",tmp)
+            result.append(tmp)
+        return result
+
+class Solution1:
+    def subsets(self, nums):
+        result = [[]]
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
+
+s=Solution()
+nums = [1,2,3]
+print(nums,s.subsets(nums))
+print([[nums[j] for j in range(len(nums)) if i >> j & 1] for i in range(2 ** len(nums))])
+#>>> print ("'%-6s'"%'hi')
+#'hi    '
+#>>> print ("'%6s'"%'hi')
+#'    hi'
