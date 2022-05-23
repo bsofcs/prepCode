43c10166714267ad9e7864abf71b83f8c6608a8e
bsofcs <42695433+bsofcs@users.noreply.github.com>
2022-05-23 09:52:27 +0530

    Update egyptianFraction.py

--- Greedy/egyptianFraction.py
+++ Greedy/egyptianFraction.py
@@ -1,6 +1,7 @@
 """
 egyptianFraction
-Every positive fraction can be represented as sum of unique unit fractions. A fraction is unit fraction if numerator is 1 and denominator is a positive integer, for example 1/3 is a unit fraction. Such a representation is called Egyptian Fraction as it was used by ancient Egyptians.
+Every positive fraction can be represented as sum of unique unit fractions. 
+A fraction is unit fraction if numerator is 1 and denominator is a positive integer, for example 1/3 is a unit fraction. Such a representation is called Egyptian Fraction as it was used by ancient Egyptians.
 Egyptian Fraction Representation of 2/3 is 1/2 + 1/6
 Egyptian Fraction Representation of 6/14 is 1/3 + 1/11 + 1/231
 Egyptian Fraction Representation of 12/13 is 1/2 + 1/3 + 1/12 + 1/156
@@ -13,6 +14,7 @@ def egyptianFraction(p,q):
 	print(p,"/",q,end=" = ")
 	tmp=math.ceil(q/p)
 	while tmp:
+		print(p,q)
 		res.append(tmp)
 		#print(p,q,tmp)
 		p=tmp*p-q
@@ -21,7 +23,7 @@ def egyptianFraction(p,q):
 		q=tmp*q
 		tmp=math.ceil(q/p)
 	for i in res:
-		print("1/",i,end=" ")
+		print("1/",i,end=", ")
 	print()
 
 p=[2,6,12,4]
